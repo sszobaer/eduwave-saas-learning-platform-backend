5979c13d8e96b2b151521e7ffb04822d1224aefb
sazzadkhan20 <sazzad53913@gmail.com>
2025-11-04 13:04:07 +0600

    Enrollment logic has been set.

--- src/modules/enrollements.module.ts
+++ src/modules/enrollements.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EnrollmentController } from 'src/controllers/enrollment.controller';
+import { EnrollmentService } from 'src/services/enrollment.service';
 
-@Module({})
+@Module({
+    controllers : [EnrollementsModule],
+    providers : [EnrollmentService]
+})
 export class EnrollementsModule {}
